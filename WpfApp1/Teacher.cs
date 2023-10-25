d2f3524c663b3b88fc233892aada656b4e31306a
SvetaKrivalova <svetakrivalova123@gmail.com>
2023-10-25 09:20:34 +0700

    /

---
+++ WpfApp1/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Teacher
+    {
+        public int id { get; set; }
+        private string last_name, first_name, middle_name;
+        public string Last_name
+        {
+            get { return last_name; }
+            set { last_name = value; }
+        }
+        public string First_name
+        {
+            get { return first_name; }
+            set { first_name = value; }
+        }
+        public string Middle_name
+        {
+            get { return middle_name; }
+            set { middle_name = value; }
+        }
+
+        public Teacher() { }
+
+        public Teacher(string last_name, string first_name, string middle_name)
+        {
+            this.last_name = last_name;
+            this.first_name = first_name;
+            this.middle_name = middle_name;
+        }
+    }
+}
